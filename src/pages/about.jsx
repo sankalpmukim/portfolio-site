6a29a6209e2a2ed914373166ef1d90b9b42db029
Sankalp Mukim <sankalpmukim@gmail.com>
2022-10-30 13:37:03 +0530

    update details

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -44,7 +44,7 @@ export default function About() {
         <title>About - Sankalp Mukim</title>
         <meta
           name="description"
-          content="I’m Sankalp Mukim. I live in New York City, where I design the future."
+          content="I’m Sankalp Mukim. I live in Delhi, where I design the future."
         />
       </Head>
       <Container className="mt-16 sm:mt-32">
@@ -61,8 +61,7 @@ export default function About() {
           </div>
           <div className="lg:order-first lg:row-span-2">
             <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-              I’m Sankalp Mukim. I live in New York City, where I design the
-              future.
+              I’m Sankalp Mukim. I live in Delhi, where I design the future.
             </h1>
             <div className="mt-6 space-y-7 text-base text-zinc-600 dark:text-zinc-400">
               <p>
