532f3c053913fb0afebf2a3aabe10b59ea1dfcb0
Sankalp Mukim <SANKALP.MUKIM@ibm.com>
2026-02-18 23:13:26 +0530

    Update hero section

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -278,13 +278,13 @@ export default function Home({ articles }) {
   return (
     <>
       <Head>
-        <title>Sankalp Mukim - Backend, Devops, Engineering</title>
+        <title>Sankalp Mukim - Tech, Engineering</title>
         <meta name="description" content={CONSTANTS.INTRO} />
       </Head>
       <Container className="mt-9">
         <div className="max-w-2xl">
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-            Backend, Devops, Engineering.
+            Tech, Engineering.
           </h1>
           <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
             {CONSTANTS.INTRO}
